8b3de761e791af2b6772af8128fa4a848c7852f3
Samuel Hulme <shulme33@gmail.com>
2019-11-17 00:18:28 -0700

    Some cleanup after repo split. The deleted files are in new repos now

--- Python/quick_sort/quick_sort.py
+++
@@ -1,5 +0,0 @@
-import sys
-
-unsorted_list = [19, 5, 17, 4, 12, 11, 22]
-
-def quick_sort(low, high):
--- WebDesign/website-temp1/src/components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-import React from "../../node_modules/react";
-import "../css/footer.css";
-
-const Footer = () => {
-  return <div className="footer"></div>;
-};
-
-export default Footer;
