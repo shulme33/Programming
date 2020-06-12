f3bcd3a464022b232b5401a78c9225ee1786d9f5
Samuel Hulme <shulme33@gmail.com>
2020-06-12 00:15:57 -0600

    Finished CCI Q1.2

--- Python/cci_2-1.py
+++ Python/cci_2-1.py
@@ -4,18 +4,30 @@
 #
 #   Write code to remove duplicates from an unsorted linked list. (Lets say its a singly linked list)
 #
-#
+#   The Algorithm:
+#       Go through the list once. As we go, if we have not seen the number before, add it to a hash table/dictionary.
+#       If we have seen it, delete the node that contains it. This algorithm's runtime is O(n).
+
+import sys
+
+##################################################
+##              Individual Nodes                ##
+##################################################
 
 class Node:
     def __init__(self, value):
         self.value = value
         self.next = None
 
+##################################################
+##               The Linked List                ##
+##################################################
+
 class LinkedList:
     def __init__(self):
         self.head = None
 
-    def insert_at_tail(self, value):
+    def insert_at_tail(self, value):    #Insert a new node at the end of the linked list
         new_node = Node(value)
 
         if self.head == None:
@@ -27,10 +39,10 @@ class LinkedList:
             current = current.next
         current.next = new_node
 
-    def remove_next_node(self, current):
+    def remove_next_node(self, current):    #Remove a node from the list
         current.next = current.next.next
 
-    def remove_duplicates(self):
+    def remove_duplicates(self):    #Remove all redundant nodes from the list
         hash = {}
         if self.head.next == None: return
 
@@ -42,40 +54,54 @@ class LinkedList:
             else:
                 current = current.next
 
-
-
-    def print_list(self):
-        print_string = "List: "
+    def print_list(self):   #Print out the list in a nice format
+        print_string = "["
         current = self.head
         while current != None:
             print_string += str(current.value) + ", "
             current = current.next
-        print(print_string)
+        print_string = print_string[:-2] + "]"
+        return print_string
 
-def create_and_populate_linked_list(values):
+
+##################################################
+##              Public Functions                ##
+##################################################
+
+def create_and_populate_linked_list(values):    #Create the linked list
     linked_list = LinkedList()
     for item in values:
         linked_list.insert_at_tail(item)
     linked_list.print_list()
     return linked_list
 
-print("Creating List...")
-linked_list = create_and_populate_linked_list([3, 3, 16, 11, 8, 14, 11, 2, 8, 16, 3, 1, 8])
-linked_list.print_list()
-print("Removing Duplicates...")
-linked_list.remove_duplicates()
-print("Duplicates Removed.")
-linked_list.print_list()
+def get_number_between(low, high, prompt):  #Get valid input. A number between "low" and "high".
+    num_items = low
+    while num_items <= low or num_items > high:
+        try:
+            num_items = int(input(prompt))
+        except:
+            num_items = low
+    return num_items
 
-print("\n\nThis program will take a list of numbers, add them to a linked list, then remove the duplicate values so that every number in the list is unique. ")
+##################################################
+##   Get input values to build the linked list  ##
+##################################################
+
+print("\nThis program will take a list of numbers, add them to a linked list, then ")
+print("remove the duplicate values so that every number in the list is unique. ")
 print("This implementation involves the use of a hash table, making the runtime O(n)")
-num_items = input("How many numbers would you like to include in your list?: ")
+num_items = get_number_between(0, 100, "\nHow many numbers, between 1 and 100, would you like to include in your list?: ")
 values = []
 for i in range(0, num_items):
-    new_num = input("Enter a number: ")
+    new_num = get_number_between(-10000, 10000, "Enter a number between +-10,000: ")
     values.append(new_num)
 
-print("Your list is: " + linked_list.print_list())
-linked_list = create_and_populate_linked_list([3, 3, 16, 11, 8, 14, 11, 2, 8, 16, 3, 1, 8])
+#################################################
+##   Create linked list and remove duplicates  ##
+#################################################
+
+linked_list = create_and_populate_linked_list(values)
+print("\nYour list is: " + linked_list.print_list())
 linked_list.remove_duplicates()
-print("Your list without duplicates: " + linked_list.print_list())
+print("Your list without duplicates: " + linked_list.print_list() + "\n")
