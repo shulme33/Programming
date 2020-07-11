89e0e3a69b1c2349cec2bb36c4a9a6b4f6486189
shulme33 <shulme33@gmail.com>
2020-07-11 13:14:49 -0600

    Finished work on PP4. Added PyCharm interpreter

--- Python/README.md
+++ Python/README.md
@@ -4,7 +4,7 @@ Download Python 3 from [Python.org](https://www.python.org/downloads/)
 
 ## To Run
 
-In the terminal, type:
+In the terminal, type something like this. You will need to alter this depending on which script you want to run. The following command will run the `cci_1-1.py` script
 
 ```bash
 python cci_1-1.py
--- Python/pp_4.py
+++ Python/pp_4.py
@@ -84,15 +84,14 @@ Samuel Hulme
     beginWord = "run"
     endWord = "car"
 
-    In the example below, a * represents a character from the endWord and a % represents a character from the level above
+    In the example below, a * represents all 26 different letters. For example, r*n = {ran, rbn, rcn....rxn, ryn, rzn}.
 
     Our tree will look like:
 
                    run
                /    |    \
         *un        r*n         ru*
-         |          |           |
-        **n        r**         *u*
+
 
 #####################################
 ##    TIME AND SPACE COMPLEXITY:   ##
@@ -102,14 +101,58 @@ Samuel Hulme
 
     The runtime is a combination of the time to look at the different iterations of the word and the time to traverse
     the tree. In the worst case, we will add every node in the dictionary to the tree. Lets call that number of nodes N.
-    The second part is going through each of the letters in the words. Lets say the words are of length M.
+    The second part is going through each of the letters in the words. Lets say the words are of length M. Also, because
+    the words list is not a dictionary to begin with, we will have to populate the dictionary with all the words in the
+    word list, leading to an additional O(N)
 
-    Therefore, the runtime is O(MN)
+    Therefore, the runtime is O(MN + N)
 
     SPACE COMPLEXITY:
 
-    Lets say we have N words in the dictionary of length M.
+    Lets say we have N words in the dictionary where each word is length M.
 
     SC = O(MN)
 
 """
+from string import ascii_lowercase
+
+def shortest_transformation(word_list, begin_word, end_word, shortest_seq=0, word_dict={}, words_seen={}, queue=[None]):
+    for word in word_list:
+        if word != begin_word: word_dict[word] = 1
+
+    if end_word not in word_list:
+        return 0
+
+    cur_b_word = begin_word
+    while len(queue) > 0:
+        if cur_b_word == end_word:
+            return shortest_seq
+
+        for i in range(0, len(cur_b_word)):
+            for letter in ascii_lowercase:
+                iteration = cur_b_word[:i] + letter + cur_b_word[i+1:]
+                if iteration in word_dict:
+                    words_seen[iteration] = 1
+                    word_dict.pop(iteration)
+                    queue.append(iteration)
+
+        queue.append(None)
+        cur_b_word = queue.pop(0)
+        if cur_b_word is None:
+            cur_b_word = queue.pop(0)
+            shortest_seq += 1
+
+
+print("\nThis program will take a list of words, a beginWord, and an endWord. ")
+print("Then, it will output the shortest path from the beginWord to the endWord that is achieved by changing")
+print("one letter of the word at a time, i.e. way -> wad -> mad --> mid. This would have a sequence of 4.")
+
+b_word = "hit"
+e_word = "cog"
+w_list = ["hot", "dot", "dog", "lot", "log", "cog"]
+
+print("\nBegin Word: " + b_word)
+print("End Word: " + e_word)
+print("Word List: " + str(w_list))
+
+print("\nShortest Path: " + str(shortest_transformation(w_list, b_word, e_word)) + "\n")
